2dbf9b5a5c124432fd1854e4cccf1b349c17335a
LordeGeorge127 <george.ken.127@gmail.com>
2023-01-31 10:42:07 +0300

    commit all to the main branch

---
+++ app1.js
@@ -0,0 +1,11 @@
+//selectors
+const todoInput = document.querySelector(".todo-input")
+const todoButton = document.querySelector(".todo-button")
+const todoList = document.querySelector(".todo-list")
+//Event Listeners
+todoButton.addEventListener('click', addTodo);
+
+//FUNCTIONS
+function addTodo(event){
+    console.log("hello")
+}
